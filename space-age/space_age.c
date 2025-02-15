6c68022bbd082ef4d2365ff25c5809ef79902849
Me1k0r <runner-me1k0r@proton.me>
2025-02-15 18:57:50 +0100

    wip: exercise - space-age
    Exercise 11

---
+++ space-age/space_age.c
@@ -0,0 +1,19 @@
+#include "space_age.h"
+#include <math.h>
+
+
+double periods[] = {
+  0.2408467,
+  0.61519726,
+  1.0,
+  1.8808158,
+  11.862615,
+  29.447498,
+  84.016846,
+  164.79132 
+};
+
+float age(planet_t planet, int64_t seconds)
+{
+  return fabs(365.25 / (365.25 * periods[planet])) * ((float)seconds / 60 / 60 / 24 / 365.25);
+}
