8bb31e0854e507c8c5e82861d8c0fc68405a8da3
Me1k0r <runner-me1k0r@proton.me>
2025-02-15 18:55:24 +0100

    new: exercise - resistor color
    Exercise 5

---
+++ resistor-color/resistor_color.c
@@ -0,0 +1,13 @@
+#include "resistor_color.h"
+
+
+unsigned int color_code(resistor_band_t col)
+{
+  return col;
+}
+
+resistor_band_t *colors()
+{
+  static resistor_band_t cols[] = { BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE };
+  return cols;
+}
