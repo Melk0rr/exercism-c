4af82ac79b6d2e88cbf0ae0ac40d785c6915ee6b
Me1k0r <runner-me1k0r@proton.me>
2025-02-18 20:53:17 +0100

    edit: exercise - rna transcript
    Exercise 12 finished

--- rna-transcription/rna_transcription.c
+++ rna-transcription/rna_transcription.c
@@ -1,20 +1,41 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include <string.h>
 
 #include "rna_transcription.h"
 
-static const char dna_to_rna[] = {
-  ['G'] = 'C',
-  ['C'] = 'G',
-  ['T'] = 'A',
-  ['A'] = 'U',
-};
-
 char *to_rna(const char *dna)
 {
-  char *rna = "";
-  for (unsigned int i = 0; i < strlen(dna); i++)
-    rna += dna_to_rna[dna[i]];
+  size_t len = strlen(dna);
+  char *rna = malloc((len + 1) * sizeof(*rna));
 
+  if (!rna)
+    return NULL;
+  
+  for (unsigned int i = 0; dna[i]; i++)
+  {
+    switch (dna[i]) {
+      case 'G':
+        rna[i] = 'C';
+        break;
+
+      case 'C':
+        rna[i] = 'G';
+        break;
+
+      case 'T':
+        rna[i] = 'A';
+        break;
+
+      case 'A':
+        rna[i] = 'U';
+        break;
+
+      default:
+        rna[i] = dna[i];
+    }
+  }
+
+  rna[len] = '\0';
   return rna;
 }
