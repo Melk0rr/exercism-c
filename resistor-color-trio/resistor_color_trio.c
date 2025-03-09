63750e806d7b3e2e072fefc9adaa7c8d6df224b9
Me1k0r <runner-me1k0r@proton.me>
2025-03-09 15:50:06 +0100

    edit: exercise - resistor color trio optimized
    Removed unused include + single value change based on unit

--- resistor-color-trio/resistor_color_trio.c
+++ resistor-color-trio/resistor_color_trio.c
@@ -1,30 +1,21 @@
-#include <stdio.h>
 #include <math.h>
 #include "resistor_color_trio.h"
 
 resistor_value_t color_code(resistor_band_t *col)
 {
   resistor_value_t resistor;
-
   resistor.value = (col[0] * 10 + col[1]) * pow(10, col[2]);
-
-  printf("Value: %lu", resistor.value);
 
   if (resistor.value < 1000)
     resistor.unit = OHMS;
-
-  else if (resistor.value < 1000000) {
+  else if (resistor.value < 1000000)
     resistor.unit = KILOOHMS;
-    resistor.value /= 1000;
-  }
-  else if (resistor.value < 1000000000) {
+  else if (resistor.value < 1000000000)
     resistor.unit = MEGAOHMS;
-    resistor.value /= 1000000;
-  }
-  else {
+  else
     resistor.unit = GIGAOHMS;
-    resistor.value /= 1000000000;
-  }
+
+  resistor.value /= pow(1000, resistor.unit);
 
   return resistor;
 }
