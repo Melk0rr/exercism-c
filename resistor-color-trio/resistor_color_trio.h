da81387729628a1a3043177c74363ed7c732b3b4
Me1k0r <runner-me1k0r@proton.me>
2025-03-09 15:36:55 +0100

    new: exercise - resistor color trio
    Exercise 21

---
+++ resistor-color-trio/resistor_color_trio.h
@@ -0,0 +1,33 @@
+#ifndef RESISTOR_COLOR_TRIO_H
+#define RESISTOR_COLOR_TRIO_H
+
+
+typedef enum {
+  BLACK,
+  BROWN,
+  RED,
+  ORANGE,
+  YELLOW,
+  GREEN,
+  BLUE,
+  VIOLET,
+  GREY,
+  WHITE,
+} resistor_band_t;
+
+typedef enum {
+  OHMS,
+  KILOOHMS,
+  MEGAOHMS,
+  GIGAOHMS
+} resistor_unit_t;
+
+typedef struct resistor_value_t
+{
+  unsigned long value;
+  resistor_unit_t unit;
+} resistor_value_t;
+
+resistor_value_t color_code(resistor_band_t *col);
+
+#endif
