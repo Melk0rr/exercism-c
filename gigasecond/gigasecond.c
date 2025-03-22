a5ed79c59b7e10c40d1f5029f312466a6fb343d1
Me1k0r <runner-me1k0r@proton.me>
2025-03-22 12:45:37 +0100

    edit: exercise/gigasecond - shorter format

--- gigasecond/gigasecond.c
+++ gigasecond/gigasecond.c
@@ -5,5 +5,5 @@
 void gigasecond(time_t input, char *output, size_t size)
 {
   time_t new_time = input + GIGASECOND;
-  strftime(output, size, "%Y-%m-%d %H:%M:%S", gmtime(&new_time));
+  strftime(output, size, "%F %T", gmtime(&new_time));
 }
