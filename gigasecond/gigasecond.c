8ee4dcc6481e083295f78358eaf9c1da8a346b6a
Me1k0r <runner-me1k0r@proton.me>
2025-03-22 12:39:52 +0100

    new: exercise/gigasecond - initial
    Exercise 26

---
+++ gigasecond/gigasecond.c
@@ -0,0 +1,9 @@
+#include "gigasecond.h"
+
+#define GIGASECOND 1000000000
+
+void gigasecond(time_t input, char *output, size_t size)
+{
+  time_t new_time = input + GIGASECOND;
+  strftime(output, size, "%Y-%m-%d %H:%M:%S", gmtime(&new_time));
+}
