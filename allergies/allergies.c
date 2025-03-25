65f88d7d427d1c0fbcee5a4fb342da12e1987fac
Me1k0r <runner-me1k0r@proton.me>
2025-03-25 21:54:49 +0100

    edit: exercise/allergies - initial commit
    Exercise 29

--- allergies/allergies.c
+++ allergies/allergies.c
@@ -1,1 +1,17 @@
 #include "allergies.h"
+
+bool is_allergic_to(allergen_t allergen, const uint16_t score) { return score & allergen; }
+
+allergen_list_t get_allergens(const uint16_t score)
+{
+  allergen_list_t all_list;
+
+  all_list.count = 0;
+  for (int i = 0; i < 8; i++)
+  {
+    all_list.allergens[1 << i] = is_allergic_to(1 << i, score);
+    if (all_list.allergens[1 << i]) 
+      all_list.count++;
+  }
+  return all_list;
+}
--- allergies/allergies.h
+++ allergies/allergies.h
@@ -2,22 +2,26 @@
 #define ALLERGIES_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 typedef enum {
-   ALLERGEN_EGGS = 0,
-   ALLERGEN_PEANUTS,
-   ALLERGEN_SHELLFISH,
-   ALLERGEN_STRAWBERRIES,
-   ALLERGEN_TOMATOES,
-   ALLERGEN_CHOCOLATE,
-   ALLERGEN_POLLEN,
-   ALLERGEN_CATS,
-   ALLERGEN_COUNT,
+   ALLERGEN_EGGS = (1 << 0),
+   ALLERGEN_PEANUTS = (1 << 1),
+   ALLERGEN_SHELLFISH = (1 << 2),
+   ALLERGEN_STRAWBERRIES = (1 << 3),
+   ALLERGEN_TOMATOES = (1 << 4),
+   ALLERGEN_CHOCOLATE = (1 << 5),
+   ALLERGEN_POLLEN = (1 << 6),
+   ALLERGEN_CATS = (1 << 7),
+   ALLERGEN_COUNT = (1 << 8),
 } allergen_t;
 
 typedef struct {
    int count;
    bool allergens[ALLERGEN_COUNT];
 } allergen_list_t;
+
+bool is_allergic_to(allergen_t allergen, const uint16_t score);
+allergen_list_t get_allergens(const uint16_t score);
 
 #endif
