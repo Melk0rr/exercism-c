473feeafcd464a75a1bbfc67bf5a06ad04005734
Me1k0r <runner-me1k0r@proton.me>
2025-04-19 15:53:42 +0200

    edit: exercism/sum_of_multiple - struct implementation for multiple function

--- sum-of-multiples/sum_of_multiples.c
+++ sum-of-multiples/sum_of_multiples.c
@@ -1,14 +1,16 @@
 #include "sum_of_multiples.h"
 
-int *multiple(unsigned int factor, unsigned int limit)
+mult_t multiple(unsigned int factor, unsigned int limit)
 {
-  int *multipleArray;
+  mult_t factor_mult;
+  factor_mult.size = 0;
   int mult = 0;
   for (unsigned int i = 1; mult < limit; i++)
   {
     mult = factor * i;
-    multipleArray[i - 1] = mult;
+    factor_mult.array[i - 1] = mult;
+    factor_mult.size++;
   }
-  return multipleArray;
+  return factor_mult;
 }
 unsigned int sum(const unsigned int *factors, const size_t number_of_factors, const unsigned int limit) {}
