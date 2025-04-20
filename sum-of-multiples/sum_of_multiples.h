e275e4c632a7fe93ede4ff184cd4d2c9ac23b27a
Me1k0r <runner-me1k0r@proton.me>
2025-04-20 17:25:33 +0200

    edit: exercise/sum-of-multiples - some function headears

--- sum-of-multiples/sum_of_multiples.h
+++ sum-of-multiples/sum_of_multiples.h
@@ -1,15 +1,19 @@
 #ifndef SUM_OF_MULTIPLES_H
 #define SUM_OF_MULTIPLES_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 typedef struct mult_t {
   unsigned int *array; 
   unsigned int size;
 } mult_t;
 
-mult_t multiple(unsigned int factor, unsigned int limit);
-mult_t merge_mult(mult_t *mults);
+void *xmalloc(size_t size);
+mult_t *new_mult_t(unsigned int mult_size);
+mult_t *multiple(unsigned int factor, unsigned int limit);
+mult_t *merge_mult(mult_t *mults[], unsigned int merge_size, const size_t number_of_factors);
+int compare_mult(const void *m1, const void *m2);
+mult_t *remove_duplicates(mult_t *merged);
 unsigned int sum(const unsigned int *factors, const size_t number_of_factors,
                  const unsigned int limit);
 
