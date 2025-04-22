9b30f6947380eb35f6b1b5e83685673a7b9d4e20
Me1k0r <runner-me1k0r@proton.me>
2025-04-22 19:11:36 +0200

    edit: exercise/clock - first implementation

--- clock/clock.c
+++ clock/clock.c
@@ -1,1 +1,29 @@
 #include "clock.h"
+#include <stdio.h>
+#include <string.h>
+
+#define MIN_PER_DAY (24 * 60)
+
+int str_to_int(char *str)
+{
+  int number;
+  sscanf(str, "%d", &number);
+
+  return number;
+}
+
+int hour(clock_t clock) { return str_to_int(clock.text); }
+int minute(clock_t clock) { return str_to_int(clock.text + 3); }
+
+clock_t clock_create(int hour, int minute)
+{
+  int total_minutes = (((hour % 24) * 60 + minute) % MIN_PER_DAY + MIN_PER_DAY) % MIN_PER_DAY;
+  clock_t new_clock;
+  sprintf(new_clock.text, "%02d:%02d", total_minutes / 60, total_minutes % 60);
+
+  return new_clock;
+}
+
+clock_t clock_add(clock_t clock, int minute_add) { return clock_create(hour(clock), minute(clock) + minute_add); }
+clock_t clock_subtract(clock_t clock, int minute_add) { return clock_create(hour(clock), minute(clock) - minute_add); }
+bool clock_is_equal(clock_t a, clock_t b) { return !strcmp(a.text, b.text); }
