d17e75d34a8a0311f5ba66fd7e722e0b53b42416
Me1k0r <runner-me1k0r@proton.me>
2025-04-27 22:59:48 +0200

    wip: exercise/rotational-cipher - ciphered string generation

--- rotational-cipher/rotational_cipher.c
+++ rotational-cipher/rotational_cipher.c
@@ -1,1 +1,15 @@
 #include "rotational_cipher.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *rotate(const char *text, int shift_key)
+{
+  unsigned int text_size = strlen(text);
+  char *rotated = malloc(text_size * sizeof(char));
+
+  // TODO: alphabet roll over
+  for (unsigned int i = 0; i < text_size; i++)
+    rotated[i] = (char)((int)text[i] + (shift_key % 26));
+
+  return rotated;
+}
