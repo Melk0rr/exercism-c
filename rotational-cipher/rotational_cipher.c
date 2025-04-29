fd2cfff167f208f317beb78028c1af1dbc1fa6af
Me1k0r <runner-me1k0r@proton.me>
2025-04-29 21:40:05 +0200

    wip: exercise/rotational-cipher - fixed char number and passing first tests

--- rotational-cipher/rotational_cipher.c
+++ rotational-cipher/rotational_cipher.c
@@ -1,15 +1,24 @@
 #include "rotational_cipher.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 char *rotate(const char *text, int shift_key)
 {
   unsigned int text_size = strlen(text);
-  char *rotated = malloc(text_size * sizeof(char));
+  char *rotated = malloc((text_size + 1) * sizeof(char));
 
   // TODO: handle upper and lower case
-  for (unsigned int i = 0; i < text_size; i++)
-    rotated[i] = (char)(((((int)text[i] - 61) + shift_key) % 26) + 61);
+  unsigned int i = 0;
+  for (; i < text_size; i++)
+  {
+    rotated[i] = ((text[i] >= 65 && text[i] <= 90) || (text[i] >= 97 && text[i] <= 122))
+                     ? (char)(((((int)text[i] - 97) + shift_key) % 26) + 97)
+                     : text[i];
+    printf("%d -> %d", text[i], rotated[i]);
+  }
+
+  rotated[i] = '\0';
 
   return rotated;
 }
