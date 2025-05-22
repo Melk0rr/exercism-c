ce568aaca6d14bb4ca360f930b03faa6442737c1
Me1k0r <runner-me1k0r@proton.me>
2025-05-22 15:41:38 +0200

    edit: exercise/square-root - minor optimization

--- square-root/square_root.c
+++ square-root/square_root.c
@@ -2,26 +2,22 @@
 
 unsigned int square_root(unsigned int num)
 {
-  if (num == 1)
-    return num;
-
-  unsigned int arr[num];
-  for (unsigned int i = 0; i < num; i++)
+  unsigned int arr[num / 2];
+  for (unsigned int i = 0; i <= num / 2; i++)
     arr[i] = i + 1;
 
-  unsigned int res, l = 0, r = num - 1;
+  unsigned int l = 0, r = (num - 1) / 2;
   while (l <= r)
   {
     unsigned int m = l + ((r - l) / 2);
-    res = arr[m];
 
-    if ((res * res) < num)
+    if ((arr[m] * arr[m]) < num)
       l = m + 1;
-    else if ((res * res) > num)
+    else if ((arr[m] * arr[m]) > num)
       r = m - 1;
     else
-      break;
+      return arr[m];
   }
 
-  return res;
+  return -1;
 }
