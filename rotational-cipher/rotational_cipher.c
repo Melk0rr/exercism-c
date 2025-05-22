12f908fe279a79fa6134c8283d3efed205938008
Me1k0r <runner-me1k0r@proton.me>
2025-05-22 15:30:33 +0200

    edit: exercise/roational-cipher - removed todo comment

--- rotational-cipher/rotational_cipher.c
+++ rotational-cipher/rotational_cipher.c
@@ -12,7 +12,6 @@ char *rotate(const char *text, int shift_key)
   unsigned int text_size = strlen(text);
   char *rotated = malloc((text_size + 1) * sizeof(char));
 
-  // TODO: handle upper and lower case
   unsigned int i = 0;
   for (; i < text_size; i++)
     rotated[i] = (text[i] >= 65 && text[i] <= 90)    ? rotate_letter(text[i], shift_key, 65)
