5280c4da6e54af74da3eac4aa779963451c5c4b3
Me1k0r <runner-me1k0r@proton.me>
2025-05-23 20:11:10 +0200

    edit: exercise/reverse-string - first version

--- reverse-string/reverse_string.c
+++ reverse-string/reverse_string.c
@@ -1,1 +1,22 @@
 #include "reverse_string.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *reverse(const char *value)
+{
+  unsigned int value_len = strlen(value);
+  char *reversed = malloc((value_len + 1) * sizeof(char));
+
+  if (reversed == NULL)
+  {
+    fprintf(stderr, "Memory allocation failed\n");
+    exit(EXIT_FAILURE);
+  }
+
+  for (unsigned int i = 0; i < value_len; i++)
+    reversed[i] = value[value_len - 1 - i];
+
+  reversed[value_len] = '\0';
+  return reversed;
+}
