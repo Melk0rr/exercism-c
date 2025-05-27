85296c74554b5bf0fc1835d1e66c9e3046ab3254
Me1k0r <runner-me1k0r@proton.me>
2025-05-27 18:02:42 +0200

    edit: exercise/nucleotide-count - final version

--- nucleotide-count/nucleotide_count.c
+++ nucleotide-count/nucleotide_count.c
@@ -3,32 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-// TODO: check malloc size (30) + strcpy
 char *count(const char *dna_strand)
 {
-  int tracker[] = {
-      ['A'] = 0,
-      ['C'] = 0,
-      ['G'] = 0,
-      ['T'] = 0,
-  };
+  int tracker[] = {['A'] = 0, ['C'] = 0, ['G'] = 0, ['T'] = 0};
+  char *count_str = malloc(30);
 
-  unsigned char invalid = 0;
   for (unsigned int i = 0; i < strlen(dna_strand); i++)
   {
     if (dna_strand[i] != 'A' && dna_strand[i] != 'C' && dna_strand[i] != 'G' && dna_strand[i] != 'T')
     {
-      invalid = 1;
-      break;
+      strcpy(count_str, "");
+      return count_str;
     }
+
     tracker[(int)dna_strand[i]]++;
   }
 
-  if (invalid == 1)
-    return calloc(1, sizeof(char));
-
-  char *count_str = malloc(strlen(dna_strand) * sizeof(char));
   sprintf(count_str, "A:%d C:%d G:%d T:%d", tracker['A'], tracker['C'], tracker['G'], tracker['T']);
-
   return count_str;
 }
