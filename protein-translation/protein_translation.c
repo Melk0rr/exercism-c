e87dd231a7b8cd95c427bbbe6ed91d17b47a40a5
Me1k0r <runner-me1k0r@proton.me>
2025-05-29 23:12:59 +0200

    wip: exercise/protein-translation - testing 3d array 3

--- protein-translation/protein_translation.c
+++ protein-translation/protein_translation.c
@@ -1,5 +1,4 @@
 #include "protein_translation.h"
-#include <stdio.h>
 #include <string.h>
 
 const amino_acid_t amino_table[86][86][86] = {
@@ -8,8 +7,6 @@ const amino_acid_t amino_table[86][86][86] = {
              ['C'] = {['A'] = Serine, ['C'] = Serine, ['G'] = Serine, ['U'] = Serine},
              ['G'] = {['A'] = -1, ['C'] = Cysteine, ['G'] = Tryptophan, ['U'] = Cysteine},
              ['U'] = {['A'] = Leucine, ['C'] = Phenylalanine, ['G'] = Leucine, ['U'] = Phenylalanine}}};
-
-unsigned int hash_codon(const char *codon) { return ((codon[0] << 4) | (codon[1] << 8) | (codon[2] << 16)) / 10000; }
 
 protein_t protein(const char *const rna)
 {
@@ -20,13 +17,7 @@ protein_t protein(const char *const rna)
     for (unsigned int i = 0; i < strlen(rna) / 3; i++)
     {
       const char *codon = rna + i * 3;
-      unsigned int codon_hash = hash_codon(codon);
-      printf("%d", codon_hash);
-      //
-      // if (codon_hash == 79 || codon_hash == 90 || codon_hash == 152)
-      //   return res;
-      //
-      // res.amino_acids[res.count++] = amino_table[codon_hash];
+      res.amino_acids[res.count++] = amino_table[(int)codon[0]][(int)codon[1]][(int)codon[2]];
     }
     res.valid = true;
   }
