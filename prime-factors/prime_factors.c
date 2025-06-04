7c95c6e00c0bd95a90103fbc7a24dbb6a424bd64
Me1k0r <runner-me1k0r@proton.me>
2025-06-04 19:55:54 +0200

    edit: exercise/prime-factors - optimized removing one loop level

--- prime-factors/prime_factors.c
+++ prime-factors/prime_factors.c
@@ -2,12 +2,16 @@
 
 size_t find_factors(uint64_t n, uint64_t factors[static MAXFACTORS])
 {
-  size_t number_of_factors = 0;
-  for (unsigned int i = 2; i < MAXFACTORS; i++)
-    while (n % i == 0) {
-      n /= i;
-      factors[number_of_factors++] = i;
+  size_t number_of_factors = 0, div = 2;
+  while (n > 1) {
+    if (n % div)
+      div++;
+
+    else {
+      factors[number_of_factors++] = div;
+      n /= div;
     }
+  }
 
   return number_of_factors;
 }
