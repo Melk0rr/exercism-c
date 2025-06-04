706ef403421122a51a7fb72bed9852a58646c45b
Me1k0r <runner-me1k0r@proton.me>
2025-06-04 19:17:08 +0200

    edit: exercise/prime-factors - simple first try (works until second to last test)

--- prime-factors/prime_factors.c
+++ prime-factors/prime_factors.c
@@ -1,1 +1,13 @@
 #include "prime_factors.h"
+
+size_t find_factors(uint64_t n, uint64_t factors[static MAXFACTORS])
+{
+  size_t number_of_factors = 0;
+  for (unsigned int i = 2; i < MAXFACTORS; i++)
+    while (n % i == 0) {
+      n /= i;
+      factors[number_of_factors++] = i;
+    }
+
+  return number_of_factors;
+}
