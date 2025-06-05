df9cc49c2b4f5914afc58360d791587f6c47bec9
Me1k0r <runner-me1k0r@proton.me>
2025-06-05 19:47:47 +0200

    edit: exercise/nth-prime - optimized

--- nth-prime/nth_prime.c
+++ nth-prime/nth_prime.c
@@ -2,18 +2,15 @@
 
 uint32_t nth(uint32_t n)
 {
-  if (n == 1)
-    return 2;
+  if (n == 0) return 0;
+  if (n == 1) return 2;
 
-  uint32_t nth = 0, count = 0;
-  for (uint32_t i = 2; count < n; i++)
+  uint32_t nth = 2, count = 1;
+  for (uint32_t i = 3; count < n; i += 2)
   {
     unsigned char flag = 0;
-    if (!(i % 2) && i != 2)
-      continue;
-
-    for (uint32_t j = 3; j <= i / 2; j++)
-      if (!(i % j))
+    for (uint32_t j = 3; j * j <= i; j+= 2)
+      if (i % j == 0)
       {
         flag = 1;
         break;
@@ -21,8 +18,8 @@ uint32_t nth(uint32_t n)
 
     if (!flag)
     {
-      count++;
       nth = i;
+      count++;
     }
   }
 
