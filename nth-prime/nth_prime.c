df7a021374e4334cad293bed286d73366b4e50c8
Me1k0r <runner-me1k0r@proton.me>
2025-06-05 19:35:49 +0200

    edit: exercise/nth-prime - completed

--- nth-prime/nth_prime.c
+++ nth-prime/nth_prime.c
@@ -1,1 +1,30 @@
 #include "nth_prime.h"
+
+uint32_t nth(uint32_t n)
+{
+  if (n == 1)
+    return 2;
+
+  uint32_t nth = 0, count = 0;
+  for (uint32_t i = 2; count < n; i++)
+  {
+    unsigned char flag = 0;
+    if (!(i % 2) && i != 2)
+      continue;
+
+    for (uint32_t j = 3; j <= i / 2; j++)
+      if (!(i % j))
+      {
+        flag = 1;
+        break;
+      }
+
+    if (!flag)
+    {
+      count++;
+      nth = i;
+    }
+  }
+
+  return nth;
+}
