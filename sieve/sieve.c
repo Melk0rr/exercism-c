3a360ce4bf746b15582db4db477eac74a92931d9
Me1k0r <runner-me1k0r@proton.me>
2025-06-05 21:40:26 +0200

    edit: exercise/sieve - optimization

--- sieve/sieve.c
+++ sieve/sieve.c
@@ -2,10 +2,17 @@
 
 uint32_t sieve(uint32_t limit, uint32_t *primes, size_t max_primes)
 {
-  uint32_t count = 0;
-  for (uint32_t i = 2; i <= limit && count < max_primes; i++) {
+  if (limit < 2) return 0;
+  if (limit == 2)
+  {
+    primes[0] = 2;
+    return 1;
+  }
+
+  uint32_t count = 1;
+  for (uint32_t i = 3; i <= limit && count < max_primes; i += 2) {
     unsigned char flag = 0;
-    for (uint32_t j = 2; j <= i / 2; j++) {
+    for (uint32_t j = 3; j * j <= i; j += 2) {
       if (i % j == 0)
       {
         flag = 1;
@@ -13,7 +20,7 @@ uint32_t sieve(uint32_t limit, uint32_t *primes, size_t max_primes)
       }
     }
 
-    if (flag == 0)
+    if (!flag)
       primes[count++] = i;
   }
 
