419e38d8cc2b890c3a51ce20cf0a5cabde108c98
Me1k0r <runner-me1k0r@proton.me>
2025-06-18 22:42:00 +0200

    wip: exercise/yacht - good until full house tests

--- yacht/yacht.c
+++ yacht/yacht.c
@@ -1,1 +1,48 @@
 #include "yacht.h"
+#include <stdlib.h>
+
+int* count_digit(dice_t dice)
+{
+  int *counts = malloc(7 * sizeof(int));
+  for (unsigned int i = 0; i < 5; i++)
+    counts[dice.faces[i]]++;
+
+  return counts;
+}
+
+int ones(dice_t dice) { return count_digit(dice)[1]; }
+int twos(dice_t dice) { return count_digit(dice)[2] * 2; }
+int threes(dice_t dice) { return count_digit(dice)[3] * 3; }
+int fours(dice_t dice) { return count_digit(dice)[4] * 4; }
+int fives(dice_t dice) { return count_digit(dice)[5] * 5; }
+int sixes(dice_t dice) { return count_digit(dice)[6] * 6; }
+int full_house(dice_t dice) {
+  int *counts = count_digit(dice);
+  unsigned char full_house = 1;
+
+  for (int i = 1; i < 7; i++)
+    if (counts[i] == 1 || counts[i] > 3)
+    {
+      full_house = 0;
+      break;
+    }
+
+  return (dice.faces[0] + dice.faces[1] + dice.faces[2] + dice.faces[3] + dice.faces[4]) * full_house;
+}
+int yacht(dice_t dice)
+{
+  return (dice.faces[0] == dice.faces[1] && dice.faces[0] == dice.faces[2] && dice.faces[0] == dice.faces[3] &&
+          dice.faces[0] == dice.faces[4])
+             ? 50
+             : 0;
+}
+
+static const struct ctg_cmd command_lookup[] = {
+    [ONES] = {ones},   [TWOS] = {twos},   [THREES] = {threes},         [FOURS] = {fours},
+    [FIVES] = {fives}, [SIXES] = {sixes}, [FULL_HOUSE] = {full_house}, [YACHT] = {yacht}};
+
+int score(dice_t dice, category_t category)
+{
+  const struct ctg_cmd *category_command = &command_lookup[category];
+  return category_command->func(dice);
+}
--- yacht/yacht.h
+++ yacht/yacht.h
@@ -1,25 +1,41 @@
 #ifndef YACHT_H
 #define YACHT_H
 
-typedef enum {
-   ONES,
-   TWOS,
-   THREES,
-   FOURS,
-   FIVES,
-   SIXES,
-   FULL_HOUSE,
-   FOUR_OF_A_KIND,
-   LITTLE_STRAIGHT,
-   BIG_STRAIGHT,
-   CHOICE,
-   YACHT
+typedef enum
+{
+  ONES,
+  TWOS,
+  THREES,
+  FOURS,
+  FIVES,
+  SIXES,
+  FULL_HOUSE,
+  FOUR_OF_A_KIND,
+  LITTLE_STRAIGHT,
+  BIG_STRAIGHT,
+  CHOICE,
+  YACHT
 } category_t;
 
-typedef struct {
-   int faces[5];
+typedef struct
+{
+  int faces[5];
 } dice_t;
 
+struct ctg_cmd
+{
+  int (*func)(dice_t dice);
+};
+
+int *count_digit(dice_t dice);
+int ones(dice_t dice);
+int twos(dice_t dice);
+int threes(dice_t dice);
+int fours(dice_t dice);
+int fives(dice_t dice);
+int sixes(dice_t dice);
+int full_house(dice_t dice);
+int yacht(dice_t dice);
 int score(dice_t dice, category_t category);
 
 #endif
