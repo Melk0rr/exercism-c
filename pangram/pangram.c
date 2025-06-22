587b00707dbc8d70cba44839d326645d9b99b820
Me1k0r <runner-me1k0r@proton.me>
2025-06-22 17:12:05 +0200

    edit: exercise/pangram - use char for to_lower_case function

--- pangram/pangram.c
+++ pangram/pangram.c
@@ -1,6 +1,6 @@
 #include "pangram.h"
 
-static int to_lower_case(char ch) { return (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch; }
+static char to_lower_case(char ch) { return (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch; }
 
 bool is_pangram(const char *sentence)
 {
@@ -9,7 +9,7 @@ bool is_pangram(const char *sentence)
 
   unsigned int letterCount[256] = {0};
   for (int i = 0; sentence[i] != '\0'; i++)
-    letterCount[to_lower_case(sentence[i])]++;
+    letterCount[(int)to_lower_case(sentence[i])]++;
 
   unsigned int count = 0;
   for (int i = 97; i < 123; i++)
