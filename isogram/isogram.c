57eb73ef68e9ed1c4fd424ca50098b2e4234bae6
Me1k0r <runner-me1k0r@proton.me>
2025-06-22 16:10:54 +0200

    edit: exercise/isogram - case consistency

--- isogram/isogram.c
+++ isogram/isogram.c
@@ -1,6 +1,6 @@
 #include "isogram.h"
 
-static int toLowerCase(int ch) { return (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch; }
+static int to_lower_case(int ch) { return (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch; }
 
 bool is_isogram(const char phrase[])
 {
@@ -10,7 +10,7 @@ bool is_isogram(const char phrase[])
   bool seen[256] = {false};
   for (int i = 0; phrase[i] != '\0'; i++)
   {
-    int letter = toLowerCase(phrase[i]);
+    int letter = to_lower_case(phrase[i]);
     if (seen[letter] == true && (letter != 32 && letter != 45))
       return false;
 
