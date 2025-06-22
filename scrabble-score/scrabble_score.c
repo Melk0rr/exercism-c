6d0e034acd76b7629b50641f73d346ae33dc0646
Me1k0r <runner-me1k0r@proton.me>
2025-06-22 16:25:38 +0200

    edit: exercise/scrabble-score - completed

--- scrabble-score/scrabble_score.c
+++ scrabble-score/scrabble_score.c
@@ -1,3 +1,18 @@
 #include "scrabble_score.h"
+#include <string.h>
 
-unsigned int score(const char *word);
+static int letter_values[] = {
+    ['a'] = 1, ['b'] = 3, ['c'] = 3, ['d'] = 2, ['e'] = 1, ['f'] = 4, ['g'] = 2, ['h'] = 4, ['i'] = 1,
+    ['j'] = 8, ['k'] = 5, ['l'] = 1, ['m'] = 3, ['n'] = 1, ['o'] = 1, ['p'] = 3, ['q'] = 10, ['r'] = 1,
+    ['s'] = 1, ['t'] = 1, ['u'] = 1, ['v'] = 4, ['w'] = 4, ['x'] = 8, ['y'] = 4, ['z'] = 10,
+};
+
+static int to_lower_case(int ch) { return (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch; }
+unsigned int score(const char *word)
+{
+  unsigned int score = 0;
+  for (unsigned int i = 0; i < strlen(word); i++)
+    score += letter_values[(int)to_lower_case(word[i])];
+
+  return score;
+}
