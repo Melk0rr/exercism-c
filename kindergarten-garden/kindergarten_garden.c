e2f34cb2995689757fba255485ef0d593edf088f
Me1k0r <runner-me1k0r@proton.me>
2025-06-27 17:57:41 +0200

    edit: exercise/kindergarten-garden - bind encoding directly in plant enum

--- kindergarten-garden/kindergarten_garden.c
+++ kindergarten-garden/kindergarten_garden.c
@@ -1,13 +1,6 @@
 #include "kindergarten_garden.h"
 #include <string.h>
 
-static const plant_t plant_encoding[] = {
-    ['C'] = CLOVER,
-    ['G'] = GRASS,
-    ['R'] = RADISHES,
-    ['V'] = VIOLETS,
-};
-
 plants_t plants(const char *diagram, const char *student)
 {
   size_t garden_row_size = (strlen(diagram) - 1) / 2;
@@ -16,7 +9,6 @@ plants_t plants(const char *diagram, const char *student)
   memcpy(garden_row2, &diagram[garden_row_size + 1], garden_row_size);
 
   unsigned char student_i = (student[0] - 65) * 2;
-  return (plants_t){
-      .plants = {plant_encoding[(int)garden_row1[student_i]], plant_encoding[(int)garden_row1[student_i + 1]],
-                 plant_encoding[(int)garden_row2[student_i]], plant_encoding[(int)garden_row2[student_i + 1]]}};
+  return (plants_t){.plants = {garden_row1[student_i], garden_row1[student_i + 1], garden_row2[student_i],
+                               garden_row2[student_i + 1]}};
 }
--- kindergarten-garden/kindergarten_garden.h
+++ kindergarten-garden/kindergarten_garden.h
@@ -1,7 +1,7 @@
 #ifndef KINDERGARTEN_GARDEN_H
 #define KINDERGARTEN_GARDEN_H
 
-typedef enum { CLOVER = 0, GRASS = 1, RADISHES = 2, VIOLETS = 3 } plant_t;
+typedef enum { CLOVER = 'C', GRASS = 'G', RADISHES = 'R', VIOLETS = 'V' } plant_t;
 
 typedef struct {
    plant_t plants[4];
