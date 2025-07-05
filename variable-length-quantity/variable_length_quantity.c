cd1e01522b6fae27be8f9fc2887f7bb082a56ccc
Me1k0r <runner-me1k0r@proton.me>
2025-07-05 18:43:17 +0200

    edit: exercise/vql - first try implementing encode function with simple base 128 convertion

--- variable-length-quantity/variable_length_quantity.c
+++ variable-length-quantity/variable_length_quantity.c
@@ -1,14 +1,43 @@
 #include "variable_length_quantity.h"
+#include <stdio.h>
 
 int encode(const uint32_t *integers, size_t integers_len, uint8_t *output)
 {
-   // write to `output`, return final output's length
-   // `output` buffer should be enough to hold the full result
+  // write to `output`, return final output's length
+  // `output` buffer should be enough to hold the full result
+  int size = 0;
+  for (size_t i = 0; i < integers_len; i++) {
+    uint8_t digits[32];
+
+    if (integers_len == 1 && integers[0] == 0)
+    {
+      output[0] = (uint8_t)0;
+      return 1;
+    }
+
+    size_t digit_i = 0;
+    uint32_t n = integers[i];
+    while(n > 0)
+    {
+      digits[digit_i++] = integers[i] % 128;
+      n /= 128;
+    }
+
+    for (int j = digit_i - 1; j >= 0; j--) {
+      printf("digit: %d", digits[j]);
+      output[size++] = digits[j];
+    }
+  }
+
+  return size;
 }
 
 int decode(const uint8_t *bytes, size_t buffer_len, uint32_t *output)
 {
-   // write to `output`, return final output's length
-   // return -1 if error
-   // `output` buffer should be enough to hold the full result
+  // write to `output`, return final output's length
+  // return -1 if error
+  // `output` buffer should be enough to hold the full result
+  output[0] = (uint8_t)0;
+  printf("(%d), %zu", bytes[0], buffer_len);
+  return 1;
 }
