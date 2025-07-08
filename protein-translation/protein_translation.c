fe35cac5882931c35586a64a9c9abacfdd613d20
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 10:35:02 +0200

    edit: exercise/protein-translation - consts + format

--- protein-translation/protein_translation.c
+++ protein-translation/protein_translation.c
@@ -5,18 +5,23 @@
 
 static const amino_acid_t amino_table[26][26][26] = {
     [0] = {[20] = {[6] = Methionine}},
-    [20] = {[0] = {[0] = -1, [2] = Tyrosine, [6] = -1, [20] = Tyrosine},
-            [2] = {[0] = Serine, [2] = Serine, [6] = Serine, [20] = Serine},
-            [6] = {[0] = -1, [2] = Cysteine, [6] = Tryptophan, [20] = Cysteine},
-            [20] = {[0] = Leucine, [2] = Phenylalanine, [6] = Leucine, [20] = Phenylalanine}}};
+    [20] = {
+        [0] = {[0] = -1, [2] = Tyrosine, [6] = -1, [20] = Tyrosine},
+        [2] = {[0] = Serine, [2] = Serine, [6] = Serine, [20] = Serine},
+        [6] = {[0] = -1, [2] = Cysteine, [6] = Tryptophan, [20] = Cysteine},
+        [20] = {[0] = Leucine,
+                [2] = Phenylalanine,
+                [6] = Leucine,
+                [20] = Phenylalanine}}};
 
-protein_t protein(const char *const rna)
+protein_t protein(const char * const rna)
 {
   protein_t res = {.valid = true, .count = 0};
-  for (unsigned int i = 0; i < strlen(rna); i += 3)
+  for (size_t i = 0; i < strlen(rna); i += 3)
   {
-    const char *codon = rna + i;
-    amino_acid_t amino = amino_table[codon[0] - UPPER_A][codon[1] - UPPER_A][codon[2] - UPPER_A];
+    char const * const codon = rna + i;
+    amino_acid_t const amino = amino_table[codon[0] - UPPER_A][codon[1] - UPPER_A]
+                                    [codon[2] - UPPER_A];
 
     if (!amino)
       res.valid = false;
--- protein-translation/protein_translation.h
+++ protein-translation/protein_translation.h
@@ -6,22 +6,24 @@
 
 #define MAX_AMINO_ACIDS 10
 
-typedef enum {
-   Methionine = 1,
-   Phenylalanine = 2,
-   Leucine = 3,
-   Serine = 4,
-   Tyrosine = 5,
-   Cysteine = 6,
-   Tryptophan = 7,
+typedef enum
+{
+  Methionine = 1,
+  Phenylalanine = 2,
+  Leucine = 3,
+  Serine = 4,
+  Tyrosine = 5,
+  Cysteine = 6,
+  Tryptophan = 7,
 } amino_acid_t;
 
-typedef struct {
-   bool valid;
-   size_t count;
-   amino_acid_t amino_acids[MAX_AMINO_ACIDS];
+typedef struct
+{
+  bool valid;
+  size_t count;
+  amino_acid_t amino_acids[MAX_AMINO_ACIDS];
 } protein_t;
 
-protein_t protein(const char *const rna);
+protein_t protein(char const * const rna);
 
 #endif
