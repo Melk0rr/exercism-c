ccdbc604dada8b344bc26ff6140d255e280f1e23
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 15:02:22 +0200

    edit: exercise/nth-prime - consts + type tweaks

--- nth-prime/nth_prime.c
+++ nth-prime/nth_prime.c
@@ -1,15 +1,17 @@
 #include "nth_prime.h"
+#include <stddef.h>
 
-uint32_t nth(uint32_t n)
+uint32_t nth(uint32_t const n)
 {
   if (n == 0) return 0;
   if (n == 1) return 2;
 
-  uint32_t nth = 2, count = 1;
-  for (uint32_t i = 3; count < n; i += 2)
+  uint32_t nth = 2;
+  size_t count = 1;
+  for (size_t i = 3; count < n; i += 2)
   {
     unsigned char flag = 0;
-    for (uint32_t j = 3; j * j <= i; j+= 2)
+    for (size_t j = 3; j * j <= i; j+= 2)
       if (i % j == 0)
       {
         flag = 1;
