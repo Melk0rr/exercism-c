a54e6ee38f05b11f8b4ec8a07cc9701f3841786a
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 12:10:21 +0200

    edit: exercise/scrabble-score - consts + size_t usage

--- scrabble-score/scrabble_score.c
+++ scrabble-score/scrabble_score.c
@@ -1,17 +1,23 @@
 #include "scrabble_score.h"
 #include <string.h>
 
-static int letter_values[] = {
-    ['a'] = 1, ['b'] = 3, ['c'] = 3, ['d'] = 2, ['e'] = 1, ['f'] = 4, ['g'] = 2, ['h'] = 4, ['i'] = 1,
-    ['j'] = 8, ['k'] = 5, ['l'] = 1, ['m'] = 3, ['n'] = 1, ['o'] = 1, ['p'] = 3, ['q'] = 10, ['r'] = 1,
-    ['s'] = 1, ['t'] = 1, ['u'] = 1, ['v'] = 4, ['w'] = 4, ['x'] = 8, ['y'] = 4, ['z'] = 10,
+static int const letter_values[] = {
+    ['a'] = 1, ['b'] = 3,  ['c'] = 3, ['d'] = 2, ['e'] = 1,  ['f'] = 4,
+    ['g'] = 2, ['h'] = 4,  ['i'] = 1, ['j'] = 8, ['k'] = 5,  ['l'] = 1,
+    ['m'] = 3, ['n'] = 1,  ['o'] = 1, ['p'] = 3, ['q'] = 10, ['r'] = 1,
+    ['s'] = 1, ['t'] = 1,  ['u'] = 1, ['v'] = 4, ['w'] = 4,  ['x'] = 8,
+    ['y'] = 4, ['z'] = 10,
 };
 
-static char to_lower_case(char ch) { return (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch; }
-unsigned int score(const char *word)
+static char to_lower_case(char const ch)
 {
-  unsigned int score = 0;
-  for (unsigned int i = 0; i < strlen(word); i++)
+  return (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch;
+}
+
+unsigned int score(const char * const word)
+{
+  size_t score = 0;
+  for (size_t i = 0; i < strlen(word); i++)
     score += letter_values[(int)to_lower_case(word[i])];
 
   return score;
