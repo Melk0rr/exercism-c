6fbfbd56546a978efcf6120f36641bf48fc4815d
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 14:39:41 +0200

    edit: exercise/collatz-conjecture - size_t usage

--- collatz-conjecture/collatz_conjecture.c
+++ collatz-conjecture/collatz_conjecture.c
@@ -1,11 +1,12 @@
 #include "collatz_conjecture.h"
+#include <stddef.h>
 
 int steps(int start)
 {
   if (start < 1)
     return ERROR_VALUE;
 
-  int count = 0;
+  size_t count = 0;
   for (; start > 1; count++)
     start = (start % 2) ? start * 3 + 1 : start / 2;
 
