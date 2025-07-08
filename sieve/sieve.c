4067bc29c3264f623a1e350f7d222812417a4045
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 15:17:20 +0200

    edit: exercise/sieve - consts + format + size_t usage

--- sieve/sieve.c
+++ sieve/sieve.c
@@ -1,6 +1,7 @@
 #include "sieve.h"
 
-uint32_t sieve(uint32_t limit, uint32_t *primes, size_t max_primes)
+uint32_t sieve(uint32_t const limit, uint32_t * const primes,
+               size_t const max_primes)
 {
   if (limit < 2) return 0;
   if (limit == 2)
@@ -9,10 +10,12 @@ uint32_t sieve(uint32_t limit, uint32_t *primes, size_t max_primes)
     return 1;
   }
 
-  uint32_t count = 1;
-  for (uint32_t i = 3; i <= limit && count < max_primes; i += 2) {
+  size_t count = 1;
+  for (size_t i = 3; i <= limit && count < max_primes; i += 2)
+  {
     unsigned char flag = 0;
-    for (uint32_t j = 3; j * j <= i; j += 2) {
+    for (size_t j = 3; j * j <= i; j += 2)
+    {
       if (i % j == 0)
       {
         flag = 1;
