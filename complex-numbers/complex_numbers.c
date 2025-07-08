ec270383a3b3dfc4d9fa5b2a81f9b8f657024462
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 14:48:05 +0200

    edit: exercise/complex-numbers - consts

--- complex-numbers/complex_numbers.c
+++ complex-numbers/complex_numbers.c
@@ -1,37 +1,48 @@
 #include "complex_numbers.h"
 #include <math.h>
 
-complex_t c_add(complex_t a, complex_t b)
+double c_real(complex_t const x) { return x.real; }
+
+double c_imag(complex_t const x) { return x.imag; }
+
+complex_t c_add(complex_t const a, complex_t const b)
 {
-  return (complex_t){.real = c_real(a) + c_real(b), .imag = c_imag(a) + c_imag(b)};
+  return (complex_t){.real = c_real(a) + c_real(b),
+                     .imag = c_imag(a) + c_imag(b)};
 }
 
-complex_t c_sub(complex_t a, complex_t b)
+complex_t c_sub(complex_t const a, complex_t const b)
 {
-  return (complex_t){.real = c_real(a) - c_real(b), .imag = c_imag(a) - c_imag(b)};
+  return (complex_t){.real = c_real(a) - c_real(b),
+                     .imag = c_imag(a) - c_imag(b)};
 }
 
-complex_t c_mul(complex_t a, complex_t b)
+complex_t c_mul(complex_t const a, complex_t const b)
 {
   return (complex_t){.real = (c_real(a) * c_real(b) - c_imag(a) * c_imag(b)),
                      .imag = (c_imag(a) * c_real(b) + c_real(a) * c_imag(b))};
 }
 
-complex_t c_div(complex_t a, complex_t b)
+complex_t c_div(complex_t const a, complex_t const b)
 {
-  return (complex_t){.real = (c_real(a) * c_real(b) + c_imag(a) * c_imag(b)) / (pow(c_real(b), 2) + pow(c_imag(b), 2)),
-                     .imag = (c_imag(a) * c_real(b) - c_real(a) * c_imag(b)) / (pow(c_real(b), 2) + pow(c_imag(b), 2))};
+  return (complex_t){.real = (c_real(a) * c_real(b) + c_imag(a) * c_imag(b)) /
+                             (pow(c_real(b), 2) + pow(c_imag(b), 2)),
+                     .imag = (c_imag(a) * c_real(b) - c_real(a) * c_imag(b)) /
+                             (pow(c_real(b), 2) + pow(c_imag(b), 2))};
 }
 
-double c_abs(complex_t x) { return sqrt(pow(c_real(x), 2) + pow(c_imag(x), 2)); }
-
-complex_t c_conjugate(complex_t x) { return (complex_t){.real = c_real(x), .imag = -c_imag(x)}; }
-
-double c_real(complex_t x) { return x.real; }
-
-double c_imag(complex_t x) { return x.imag; }
-
-complex_t c_exp(complex_t x)
+double c_abs(complex_t const x)
 {
-  return (complex_t){.real = exp(c_real(x)) * cos(c_imag(x)), .imag = exp(c_real(x)) * sin(c_imag(x))};
+  return sqrt(pow(c_real(x), 2) + pow(c_imag(x), 2));
+}
+
+complex_t c_conjugate(complex_t const x)
+{
+  return (complex_t){.real = c_real(x), .imag = -c_imag(x)};
+}
+
+complex_t c_exp(complex_t const x)
+{
+  return (complex_t){.real = exp(c_real(x)) * cos(c_imag(x)),
+                     .imag = exp(c_real(x)) * sin(c_imag(x))};
 }
