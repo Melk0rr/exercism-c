47dba169711334e072ba8025f58496b2cd4cb987
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 14:05:25 +0200

    edit: exercise/two-fer - consts

--- two-fer/two_fer.c
+++ two-fer/two_fer.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include "two_fer.h"
+#include <stdio.h>
 
-void two_fer(char *buffer, const char *name)
+void two_fer(char * const buffer, char const * const name)
 {
   sprintf(buffer, "One for %s, one for me.", name ? name : "you");
 }
--- two-fer/two_fer.h
+++ two-fer/two_fer.h
@@ -1,6 +1,6 @@
 #ifndef TWO_FER_H
 #define TWO_FER_H
 
-void two_fer(char *buffer, const char *name);
+void two_fer(char * const buffer, char const * const name);
 
 #endif
