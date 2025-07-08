783d96a6adb81a2851050c4a318fd1c13e2de8bc
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 10:08:24 +0200

    edit: exercise/hamming - consts

--- hamming/hamming.c
+++ hamming/hamming.c
@@ -1,17 +1,16 @@
 #include "hamming.h"
 #include <string.h>
 
-int compute(const char *lhs, const char *rhs)
+int compute(char const * const lhs, char const * const rhs)
 {
-  if (strlen(lhs) != strlen(rhs))
+  unsigned int const lhs_len = strlen(lhs), rhs_len = strlen(rhs);
+  if (lhs_len != rhs_len)
     return -1;
 
   int ham_dist = 0;
-
-  for (unsigned int i = 0; i < strlen(lhs); i++)
+  for (unsigned int i = 0; i < lhs_len; i++)
     if (lhs[i] != rhs[i])
       ++ham_dist;
 
   return ham_dist;
 }
-
--- hamming/hamming.h
+++ hamming/hamming.h
@@ -1,6 +1,6 @@
 #ifndef HAMMING_H
 #define HAMMING_H
 
-int compute(const char *lhs, const char *rhs);
+int compute(char const * const lhs, char const * const rhs);
 
 #endif
