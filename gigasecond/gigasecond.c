0be2178f15178d82ffca3a0565b1037819a0fedc
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 14:58:35 +0200

    edit: exercise/gigasecond - missing const

--- gigasecond/gigasecond.c
+++ gigasecond/gigasecond.c
@@ -4,6 +4,6 @@
 
 void gigasecond(time_t const input, char * const output, size_t const size)
 {
-  time_t new_time = input + GIGASECOND;
+  time_t const new_time = input + GIGASECOND;
   strftime(output, size, "%F %T", gmtime(&new_time));
 }
