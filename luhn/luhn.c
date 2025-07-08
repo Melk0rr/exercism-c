acbba5b5c7add8cdae90ec26c90caf6cd83e681f
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 13:27:36 +0200

    edit: exercise/luhn - format + size_t usage

--- luhn/luhn.c
+++ luhn/luhn.c
@@ -1,18 +1,25 @@
+#include "luhn.h"
 #include <string.h>
 
-#include "luhn.h"
-
-static bool is_valid_char(char const cc) { return (cc == 32) || (cc >= 48 && cc <= 57); }
-static int double_digit(int const digit) { return (digit * 2 > 9) ? digit * 2 - 9 : digit * 2; }
+static bool is_valid_char(char const cc)
+{
+  return (cc == 32) || (cc >= 48 && cc <= 57);
+}
+static int double_digit(int const digit)
+{
+  return (digit * 2 > 9) ? digit * 2 - 9 : digit * 2;
+}
 
 bool luhn(char const * const num)
 {
-  if (strlen(num) <= 1)
+  size_t const num_len = strlen(num);
+  if (num_len <= 1)
     return false;
 
-  char doubled_num[strlen(num)];
-  unsigned int sum = 0, digit_counter = 0;
-  for (int i = strlen(num) - 1; i >= 0; i--)
+  char doubled_num[num_len];
+  size_t sum = 0;
+  size_t digit_counter = 0;
+  for (int i = num_len - 1; i >= 0; i--)
   {
     if (!is_valid_char(num[i]))
       return false;
@@ -21,7 +28,7 @@ bool luhn(char const * const num)
     if (num[i] != 32)
     {
       unsigned int doubled = num[i] - '0';
-      if (++digit_counter % 2 == 0) 
+      if (++digit_counter % 2 == 0)
         doubled = double_digit(doubled);
 
       doubled_num[i] = (char)(doubled + '0');
