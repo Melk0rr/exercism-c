a5d01a7e9bd9320f800d843c0e4972d42cabedab
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 12:26:21 +0200

    edit: exercise/reverse-string - consts + size_t usage

--- reverse-string/reverse_string.c
+++ reverse-string/reverse_string.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *reverse(const char *value)
+char *reverse(char const * const value)
 {
-  unsigned int value_len = strlen(value);
-  char *reversed = malloc((value_len + 1) * sizeof(char));
+  size_t const value_len = strlen(value);
+  char * const reversed = malloc((value_len + 1) * sizeof(char));
 
   if (!reversed)
     exit(EXIT_FAILURE);
 
-  for (unsigned int i = 0; i < value_len; i++)
+  for (size_t i = 0; i < value_len; i++)
     reversed[i] = value[value_len - 1 - i];
 
   reversed[value_len] = '\0';
