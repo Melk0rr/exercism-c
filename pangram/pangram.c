740adf1979d122e234c4f7357ce9bd91ad0c9527
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 14:05:11 +0200

    edit: exercise/pangram - consts + size_t usage

--- pangram/pangram.c
+++ pangram/pangram.c
@@ -1,18 +1,23 @@
 #include "pangram.h"
+#include <stddef.h>
+#include <stdint.h>
 
-static char to_lower_case(char ch) { return (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch; }
+static char const to_lower_case(char const ch)
+{
+  return (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch;
+}
 
-bool is_pangram(const char *sentence)
+bool is_pangram(char const * const sentence)
 {
   if (!sentence)
     return false;
 
-  unsigned int letterCount[256] = {0};
-  for (int i = 0; sentence[i] != '\0'; i++)
+  uint8_t letterCount[256] = {0};
+  for (size_t i = 0; sentence[i] != '\0'; i++)
     letterCount[(int)to_lower_case(sentence[i])]++;
 
-  unsigned int count = 0;
-  for (int i = 97; i < 123; i++)
+  size_t count = 0;
+  for (size_t i = 97; i < 123; i++)
     if (letterCount[i] > 0)
       ++count;
 
