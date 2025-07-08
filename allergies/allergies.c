20b3ac600b559e6570ebc6e888722ba4d66b15a7
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 14:31:27 +0200

    edit: exercise/allergies - consts + size_t usage

--- allergies/allergies.c
+++ allergies/allergies.c
@@ -1,17 +1,22 @@
 #include "allergies.h"
+#include <stddef.h>
 
-bool is_allergic_to(allergen_t allergen, const uint16_t score) { return score & allergen; }
+bool is_allergic_to(allergen_t const allergen, uint16_t const score)
+{
+  return score & allergen;
+}
 
-allergen_list_t get_allergens(const uint16_t score)
+allergen_list_t get_allergens(uint16_t const score)
 {
   allergen_list_t all_list;
 
   all_list.count = 0;
-  for (int i = 0; i < 8; i++)
+  for (size_t i = 0; i < 8; i++)
   {
     all_list.allergens[1 << i] = is_allergic_to(1 << i, score);
-    if (all_list.allergens[1 << i]) 
+    if (all_list.allergens[1 << i])
       all_list.count++;
   }
+
   return all_list;
 }
--- allergies/allergies.h
+++ allergies/allergies.h
@@ -4,24 +4,26 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef enum {
-   ALLERGEN_EGGS = (1 << 0),
-   ALLERGEN_PEANUTS = (1 << 1),
-   ALLERGEN_SHELLFISH = (1 << 2),
-   ALLERGEN_STRAWBERRIES = (1 << 3),
-   ALLERGEN_TOMATOES = (1 << 4),
-   ALLERGEN_CHOCOLATE = (1 << 5),
-   ALLERGEN_POLLEN = (1 << 6),
-   ALLERGEN_CATS = (1 << 7),
-   ALLERGEN_COUNT = (1 << 8),
+typedef enum
+{
+  ALLERGEN_EGGS = (1 << 0),
+  ALLERGEN_PEANUTS = (1 << 1),
+  ALLERGEN_SHELLFISH = (1 << 2),
+  ALLERGEN_STRAWBERRIES = (1 << 3),
+  ALLERGEN_TOMATOES = (1 << 4),
+  ALLERGEN_CHOCOLATE = (1 << 5),
+  ALLERGEN_POLLEN = (1 << 6),
+  ALLERGEN_CATS = (1 << 7),
+  ALLERGEN_COUNT = (1 << 8),
 } allergen_t;
 
-typedef struct {
-   int count;
-   bool allergens[ALLERGEN_COUNT];
+typedef struct
+{
+  int count;
+  bool allergens[ALLERGEN_COUNT];
 } allergen_list_t;
 
-bool is_allergic_to(allergen_t allergen, const uint16_t score);
-allergen_list_t get_allergens(const uint16_t score);
+bool is_allergic_to(allergen_t const allergen, uint16_t const score);
+allergen_list_t get_allergens(uint16_t const score);
 
 #endif
