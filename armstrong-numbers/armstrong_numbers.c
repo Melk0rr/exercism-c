57887cf9f023d3c8e7675977b4af0d390529c7c4
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 14:35:18 +0200

    edit: exercise/armstrong-numbers - consts + type tweaks

--- armstrong-numbers/armstrong_numbers.c
+++ armstrong-numbers/armstrong_numbers.c
@@ -1,16 +1,17 @@
-#include <math.h>
 #include "armstrong_numbers.h"
+#include <math.h>
+#include <stdint.h>
 
 bool is_armstrong_number(int candidate)
 {
   int sum = candidate;
-  unsigned int n = floor(log10(candidate) + 1);
+  uint8_t const n = floor(log10(candidate) + 1);
 
   while (candidate)
   {
     sum -= pow(candidate % 10, n);
     candidate /= 10;
   }
-  
+
   return sum == 0;
 }
