2922660e5a0dd18ef5afac97d57d739b99d2d353
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 15:07:23 +0200

    edit: exercise/queen-attack - format

--- queen-attack/queen_attack.c
+++ queen-attack/queen_attack.c
@@ -3,15 +3,14 @@
 
 attack_status_t can_attack(position_t const queen_1, position_t const queen_2)
 {
-  return 
-  (
-    (queen_1.row > 7 || queen_1.column > 7) ||
-    (queen_2.row > 7 || queen_2.column > 7) ||
-    (queen_1.row == queen_2.row && queen_1.column == queen_2.column)
-  ) ? INVALID_POSITION :
-  (
-    (queen_1.column == queen_2.column) ||
-    (queen_1.row == queen_2.row) ||
-    (abs(queen_1.row - queen_2.row) == abs(queen_1.column - queen_2.column))
-  ) ? CAN_ATTACK : CAN_NOT_ATTACK;
+  return ((queen_1.row > 7 || queen_1.column > 7) ||
+          (queen_2.row > 7 || queen_2.column > 7) ||
+          (queen_1.row == queen_2.row && queen_1.column == queen_2.column))
+             ? INVALID_POSITION
+         : ((queen_1.column == queen_2.column) ||
+            (queen_1.row == queen_2.row) ||
+            (abs(queen_1.row - queen_2.row) ==
+             abs(queen_1.column - queen_2.column)))
+             ? CAN_ATTACK
+             : CAN_NOT_ATTACK;
 }
