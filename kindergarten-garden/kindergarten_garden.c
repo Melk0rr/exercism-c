137c486a0c2fcdc2af63ad7fd6e89f60fe002c71
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 13:22:41 +0200

    edit: exercise/kindergarten-garden - consts + format

--- kindergarten-garden/kindergarten_garden.c
+++ kindergarten-garden/kindergarten_garden.c
@@ -1,14 +1,14 @@
 #include "kindergarten_garden.h"
 #include <string.h>
 
-plants_t plants(const char *diagram, const char *student)
+plants_t plants(char const * const diagram, char const * const student)
 {
-  size_t garden_row_size = (strlen(diagram) - 1) / 2;
+  size_t const garden_row_size = (strlen(diagram) - 1) / 2;
   char garden_row1[garden_row_size], garden_row2[garden_row_size];
   memcpy(garden_row1, &diagram[0], garden_row_size);
   memcpy(garden_row2, &diagram[garden_row_size + 1], garden_row_size);
 
-  unsigned char student_i = (student[0] - 65) * 2;
+  unsigned char const student_i = (student[0] - 65) * 2;
   return (plants_t){.plants = {garden_row1[student_i], garden_row1[student_i + 1], garden_row2[student_i],
                                garden_row2[student_i + 1]}};
 }
