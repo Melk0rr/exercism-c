0033b0c1fb37ba694a5990d73c22193e42823623
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 15:30:17 +0200

    edit: exercise/triangle - consts

--- triangle/triangle.c
+++ triangle/triangle.c
@@ -1,22 +1,27 @@
 #include "triangle.h"
 
-bool is_triangle(triangle_t triangle)
+bool is_triangle(triangle_t const triangle)
 {
-  return !(triangle.a == 0 || triangle.b == 0 || triangle.c == 0) && (triangle.a + triangle.b >= triangle.c) &&
-         (triangle.b + triangle.c >= triangle.a) && (triangle.a + triangle.c >= triangle.b);
+  return !(triangle.a == 0 || triangle.b == 0 || triangle.c == 0) &&
+         (triangle.a + triangle.b >= triangle.c) &&
+         (triangle.b + triangle.c >= triangle.a) &&
+         (triangle.a + triangle.c >= triangle.b);
 }
 
-bool is_equilateral(triangle_t triangle)
+bool is_equilateral(triangle_t const triangle)
 {
-  return is_triangle(triangle) && (triangle.a == triangle.b && triangle.b == triangle.c);
+  return is_triangle(triangle) &&
+         (triangle.a == triangle.b && triangle.b == triangle.c);
 }
 
-bool is_isosceles(triangle_t triangle)
+bool is_isosceles(triangle_t const triangle)
 {
-  return is_triangle(triangle) && (triangle.a == triangle.b || triangle.a == triangle.c || triangle.b == triangle.c);
+  return is_triangle(triangle) &&
+         (triangle.a == triangle.b || triangle.a == triangle.c ||
+          triangle.b == triangle.c);
 }
 
-bool is_scalene(triangle_t triangle)
+bool is_scalene(triangle_t const triangle)
 {
   return is_triangle(triangle) && !(is_isosceles(triangle));
 }
--- triangle/triangle.h
+++ triangle/triangle.h
@@ -3,15 +3,16 @@
 
 #include <stdbool.h>
 
-typedef struct {
-   double a;
-   double b;
-   double c;
+typedef struct
+{
+  double a;
+  double b;
+  double c;
 } triangle_t;
 
-bool is_triangle(triangle_t triangle);
-bool is_equilateral(triangle_t triangle);
-bool is_isosceles(triangle_t triangle);
-bool is_scalene(triangle_t triangle);
+bool is_triangle(triangle_t const triangle);
+bool is_equilateral(triangle_t const triangle);
+bool is_isosceles(triangle_t const triangle);
+bool is_scalene(triangle_t const triangle);
 
 #endif
