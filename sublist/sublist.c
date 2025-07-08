da0a572b60b5b7f74649414124c933e16d04ea2a
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 15:29:09 +0200

    edit: exercise/sublist - consts + format

--- sublist/sublist.c
+++ sublist/sublist.c
@@ -1,19 +1,20 @@
 #include "sublist.h"
 #include <stdbool.h>
 
-static bool is_sublist(int *list_to_compare, int *base_list, size_t list_to_compare_element_count,
-                       size_t base_list_element_count)
+static bool is_sublist(int const * const list_to_compare,
+                       int const * const base_list,
+                       size_t const list_to_compare_element_count,
+                       size_t const base_list_element_count)
 {
-  if (list_to_compare_element_count == 0)
-    return true;
+  if (list_to_compare_element_count == 0) return true;
+  if (list_to_compare_element_count > base_list_element_count) return false;
 
-  if (list_to_compare_element_count > base_list_element_count)
-    return false;
-
-  for (size_t i = 0; i <= base_list_element_count - list_to_compare_element_count; i++)
+  for (size_t i = 0;
+       i <= base_list_element_count - list_to_compare_element_count; i++)
   {
     size_t j = 0;
-    while (j < list_to_compare_element_count && base_list[i + j] == list_to_compare[j])
+    while (j < list_to_compare_element_count &&
+           base_list[i + j] == list_to_compare[j])
       ++j;
 
     if (j == list_to_compare_element_count)
@@ -22,8 +23,10 @@ static bool is_sublist(int *list_to_compare, int *base_list, size_t list_to_comp
 
   return false;
 }
-comparison_result_t check_lists(int *list_to_compare, int *base_list, size_t list_to_compare_element_count,
-                                size_t base_list_element_count)
+comparison_result_t check_lists(int const * const list_to_compare,
+                                int const * const base_list,
+                                size_t const list_to_compare_element_count,
+                                size_t const base_list_element_count)
 {
   if (list_to_compare_element_count == base_list_element_count)
   {
@@ -37,14 +40,15 @@ comparison_result_t check_lists(int *list_to_compare, int *base_list, size_t lis
       }
     }
 
-    if (equal)
-      return EQUAL;
+    if (equal) return EQUAL;
   }
 
-  if (is_sublist(list_to_compare, base_list, list_to_compare_element_count, base_list_element_count))
+  if (is_sublist(list_to_compare, base_list, list_to_compare_element_count,
+                 base_list_element_count))
     return SUBLIST;
 
-  if (is_sublist(base_list, list_to_compare, base_list_element_count, list_to_compare_element_count))
+  if (is_sublist(base_list, list_to_compare, base_list_element_count,
+                 list_to_compare_element_count))
     return SUPERLIST;
 
   return UNEQUAL;
--- sublist/sublist.h
+++ sublist/sublist.h
@@ -11,7 +11,9 @@ typedef enum
   SUPERLIST
 } comparison_result_t;
 
-comparison_result_t check_lists(int *list_to_compare, int *base_list, size_t list_to_compare_element_count,
-                                size_t base_list_element_count);
+comparison_result_t check_lists(int const * const list_to_compare,
+                                int const * const base_list,
+                                size_t const list_to_compare_element_count,
+                                size_t const base_list_element_count);
 
 #endif
