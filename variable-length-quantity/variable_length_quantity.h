3c124073fe34740803b7a92248497e1424ac98d4
Me1k0r <runner-me1k0r@proton.me>
2025-07-08 15:31:29 +0200

    edit: exercise/vql - removed useless comments

--- variable-length-quantity/variable_length_quantity.h
+++ variable-length-quantity/variable_length_quantity.h
@@ -4,16 +4,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
-/// Encode an array of integers into Variable Length Quantity.
-///
-/// The function returns the size of the output buffer used.
 int encode(uint32_t const * const integers, size_t const integers_len,
            uint8_t * const output);
-
-/// Decode a Variable Length Quantity byte array into an array of integers.
-///
-/// The function returns the size of the output buffer used or -1 if an error
-/// occurred.
 int decode(uint8_t const * const bytes, size_t const buffer_len,
            uint32_t * const output);
 
