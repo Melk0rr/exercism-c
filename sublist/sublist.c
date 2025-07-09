592393fc5f379c12c80b5a6701a22c7f1732bd9d
Me1k0r <runner-me1k0r@proton.me>
2025-07-09 11:57:55 +0200

    edit: exercise/sublist - separate function to test if lists are equal to reduce scopes

--- sublist/sublist.c
+++ sublist/sublist.c
@@ -6,8 +6,10 @@ static bool is_sublist(int const * const list_to_compare,
                        size_t const list_to_compare_element_count,
                        size_t const base_list_element_count)
 {
-  if (list_to_compare_element_count == 0) return true;
-  if (list_to_compare_element_count > base_list_element_count) return false;
+  if (list_to_compare_element_count == 0)
+    return true;
+  if (list_to_compare_element_count > base_list_element_count)
+    return false;
 
   for (size_t i = 0;
        i <= base_list_element_count - list_to_compare_element_count; i++)
@@ -23,33 +25,42 @@ static bool is_sublist(int const * const list_to_compare,
 
   return false;
 }
+
+static bool is_equal(int const * const list_to_compare,
+                     int const * const base_list,
+                     size_t const list_to_compare_element_count,
+                     size_t const base_list_element_count)
+{
+  if (list_to_compare_element_count != base_list_element_count)
+    return false;
+
+  bool equal = true;
+  for (size_t i = 0; i < base_list_element_count; i++)
+  {
+    if (list_to_compare[i] != base_list[i])
+    {
+      equal = false;
+      break;
+    }
+  }
+
+  return equal;
+}
+
 comparison_result_t check_lists(int const * const list_to_compare,
                                 int const * const base_list,
                                 size_t const list_to_compare_element_count,
                                 size_t const base_list_element_count)
 {
-  if (list_to_compare_element_count == base_list_element_count)
-  {
-    bool equal = true;
-    for (size_t i = 0; i < base_list_element_count; i++)
-    {
-      if (list_to_compare[i] != base_list[i])
-      {
-        equal = false;
-        break;
-      }
-    }
 
-    if (equal) return EQUAL;
-  }
-
-  if (is_sublist(list_to_compare, base_list, list_to_compare_element_count,
-                 base_list_element_count))
-    return SUBLIST;
-
-  if (is_sublist(base_list, list_to_compare, base_list_element_count,
-                 list_to_compare_element_count))
-    return SUPERLIST;
-
-  return UNEQUAL;
+  return is_equal(base_list, list_to_compare, base_list_element_count,
+                  list_to_compare_element_count)
+             ? EQUAL
+         : is_sublist(list_to_compare, base_list,
+                      list_to_compare_element_count, base_list_element_count)
+             ? SUBLIST
+         : is_sublist(base_list, list_to_compare, base_list_element_count,
+                      list_to_compare_element_count)
+             ? SUPERLIST
+             : UNEQUAL;
 }
