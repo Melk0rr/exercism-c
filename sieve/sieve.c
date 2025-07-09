485ce85376dfc7f8f9c4c986b03befdae83deb54
Me1k0r <runner-me1k0r@proton.me>
2025-07-09 11:06:31 +0200

    edit: exercise/sieve - use stdbool for flag

--- sieve/sieve.c
+++ sieve/sieve.c
@@ -1,4 +1,5 @@
 #include "sieve.h"
+#include <stdbool.h>
 
 uint32_t sieve(uint32_t const limit, uint32_t * const primes,
                size_t const max_primes)
@@ -13,12 +14,12 @@ uint32_t sieve(uint32_t const limit, uint32_t * const primes,
   size_t count = 1;
   for (size_t i = 3; i <= limit && count < max_primes; i += 2)
   {
-    unsigned char flag = 0;
+    bool flag = false;
     for (size_t j = 3; j * j <= i; j += 2)
     {
       if (i % j == 0)
       {
-        flag = 1;
+        flag = true;
         break;
       }
     }
