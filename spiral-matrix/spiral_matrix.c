204648e5f05930d6f007a03645f18306e32a3ef4
Me1k0r <runner-me1k0r@proton.me>
2025-08-09 18:34:03 +0200

    edit: exercise/spiral-matrix - first working version

--- spiral-matrix/spiral_matrix.c
+++ spiral-matrix/spiral_matrix.c
@@ -1,1 +1,85 @@
 #include "spiral_matrix.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void * xmalloc(size_t const size, char const * const err)
+{
+  void * p = malloc(size);
+
+  if (!p)
+  {
+    fprintf(stderr, "%s\n", err);
+    exit(EXIT_FAILURE);
+  }
+
+  return p;
+}
+
+static spiral_matrix_t * init_matrix(size_t const matrix_size)
+{
+  spiral_matrix_t * const new_spiral = xmalloc(
+      sizeof(*new_spiral), "Memory allocation failed for new spiral matrix");
+
+  new_spiral->size = matrix_size;
+
+  if (matrix_size > 0)
+  {
+    new_spiral->matrix = xmalloc(matrix_size * sizeof(*new_spiral->matrix),
+                                 "Memory allocation failed for spiral matrix");
+
+    for (size_t i = 0; i < matrix_size; i++)
+      new_spiral->matrix[i] =
+          calloc(matrix_size, sizeof(*new_spiral->matrix[i]));
+  }
+
+  return new_spiral;
+}
+
+spiral_matrix_t * spiral_matrix_create(int const matrix_size)
+{
+  spiral_matrix_t * const new_spiral = init_matrix(matrix_size);
+
+  int8_t dx = 1, dy = 0;
+  int x = 0, y = 0;
+  for (size_t i = 0; i < matrix_size * matrix_size; i++)
+  {
+    new_spiral->matrix[y][x] = i + 1;
+
+    if (
+        // right boundary
+        (x + dx >= matrix_size) ||
+
+        // bottom boundary
+        (y - dy >= matrix_size) ||
+
+        // left boundary
+        (x + dx < 0) ||
+
+        // top boundary
+        (y - dy < 0) ||
+
+        // next index is already set
+        (new_spiral->matrix[y - dy][x + dx]))
+    {
+      int8_t temp_dx = dx;
+      dx = dy;
+      dy = -temp_dx;
+    }
+
+    x += dx;
+    y -= dy;
+  }
+
+  return new_spiral;
+}
+
+void spiral_matrix_destroy(spiral_matrix_t * const spiral)
+{
+  for (size_t i = 0; i < spiral->size; i++)
+    free(spiral->matrix[i]);
+
+  free(spiral->matrix);
+  free(spiral);
+}
