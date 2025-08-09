b0df8f5f703e7a8fc217b113f815ef3ec9a58e0c
Me1k0r <runner-me1k0r@proton.me>
2025-08-09 16:03:07 +0200

    edit: exercise/spiral-matrix - format + headers

--- spiral-matrix/spiral_matrix.h
+++ spiral-matrix/spiral_matrix.h
@@ -1,9 +1,14 @@
 #ifndef SPIRAL_MATRIX_H
 #define SPIRAL_MATRIX_H
 
-typedef struct {
-   int size;
-   int **matrix;
+#include <stddef.h>
+typedef struct
+{
+  int size;
+  int ** matrix;
 } spiral_matrix_t;
+
+spiral_matrix_t * spiral_matrix_create(size_t const matrix_size);
+void spiral_matrix_destroy(spiral_matrix_t const * const matrix);
 
 #endif
